36250d6c21df3fa6b14756ec69972ce58e6e5385
MC_cubes <mccube2000@outlook.com>
2022-09-03 23:33:48 +0800

    format asm & add vsc settings.json

--- src/boot/x86/mbr.asm
+++ src/boot/x86/mbr.asm
@@ -18,7 +18,7 @@ section mbr vstart=0x7c00
     mov sp, 0x7c00
 
 ; 保存led信息
-    mov ah,0x02
+    mov ah, 0x02
     int 0x16                        ; keyboard BIOS
     mov [leds], al
 
@@ -47,22 +47,22 @@ section mbr vstart=0x7c00
     ; 跳过0#号描述符的槽位
 
     ; 创建1#描述符，保护模式下的代码段描述符
-    mov dword [ebx + 0x08], 0x0000_ffff      ; 基地址为0，界限0xFFFFF，DPL=00
-    mov dword [ebx + 0x0c], 0x00cf_9800      ; 4KB粒度，代码段描述符，向上扩展
+    mov dword [ebx + 0x08], 0x0000_ffff         ; 基地址为0，界限0xFFFFF，DPL=00
+    mov dword [ebx + 0x0c], 0x00cf_9800         ; 4KB粒度，代码段描述符，向上扩展
 
     ; 创建2#描述符，保护模式下的数据段和堆栈段描述符 
-    mov dword [ebx + 0x10], 0x0000_ffff      ; 基地址为0，界限0xFFFFF，DPL=00
-    mov dword [ebx + 0x14], 0x00cf_9200      ; 4KB粒度，数据段描述符，向上扩展
+    mov dword [ebx + 0x10], 0x0000_ffff         ; 基地址为0，界限0xFFFFF，DPL=00
+    mov dword [ebx + 0x14], 0x00cf_9200         ; 4KB粒度，数据段描述符，向上扩展
 
     ; 初始化描述符表寄存器GDTR
-    mov word [cs: pgdt], 23                 ; 描述符表的界限
+    mov word [cs: pgdt], 23         ; 描述符表的界限
 
     lgdt [cs: pgdt]
 
 ; A20
-    in al, 0x92                      ; 南桥芯片内的端口
+    in al, 0x92                     ; 南桥芯片内的端口
     or al, 0000_0010b
-    out 0x92, al                     ; 打开A20
+    out 0x92, al                    ; 打开A20
 
 ; 关闭中断
     cli                             ; 还未进行中断处理,暂时关闭中断
@@ -74,7 +74,7 @@ section mbr vstart=0x7c00
 
 ; 进入保护模式
     ; 清流水线并串行化处理器
-    jmp dword 0x0008:flush          ; 16位的描述符选择子：32位偏移
+    jmp dword 0x0008: flush         ; 16位的描述符选择子：32位偏移
 
 [bits 32]
 
@@ -87,8 +87,87 @@ flush:
     mov ss, eax                     ; 加载堆栈段(4GB)选择子
     mov esp, 0x7000                 ; 堆栈指针
 
-pgdt    dw 0
-        dd 0x00008000     ;GDT的物理/线性地址
+;以下加载系统核心程序
+    mov edi, core_base_address
+    mov eax, core_start_sector
+    mov ebx, edi                    ; 起始地址
+    call read_hard_disk_0           ; 以下读取程序的起始部分（一个扇区）
 
-times   510-($-$$)  db 0
+    ; 以下判断整个程序有多大
+    mov eax, [edi]                  ; 核心程序尺寸
+    xor edx, edx
+    mov ecx, 512                    ; 512字节每扇区
+    div ecx
+
+    or edx, edx
+    jnz @1                          ; 未除尽，因此结果比实际扇区数少1
+    dec eax                         ; 已经读了一个扇区，扇区总数减1
+    @1:
+        or eax, eax                 ; 考虑实际长度≤512个字节的情况
+        jz pge                      ; EAX=0 ?
+
+    ; 读取剩余的扇区
+    mov ecx, eax                    ;32位模式下的LOOP使用ECX
+    mov eax,core_start_sector
+    inc eax                         ; 从下一个逻辑扇区接着读
+    @2:
+        call read_hard_disk_0
+        inc eax
+        loop @2                     ; 循环读，直到读完整个内核
+
+; 开启分页
+    pge:
+        ; 创建系统内核的页目录表PDT
+        mov ebx, 0x00020000         ; 页目录表PDT的物理地址
+
+        ; 在页目录内创建指向页目录表自己的目录项
+        mov dword [ebx + 4092], 0x00020003
+        mov edx, 0x00021003         ; MBR空间有限，后面尽量不使用立即数
+
+        ; 在页目录内创建与线性地址0x00000000对应的目录项
+        mov [ebx + 0x000], edx      ; 写入目录项（页表的物理地址和属性）      
+                                    ; 此目录项仅用于过渡。
+
+        ; 在页目录内创建与线性地址0x80000000对应的目录项
+        mov [ebx + 0x800], edx      ;写入目录项（页表的物理地址和属性）
+
+        ;创建与上面那个目录项相对应的页表，初始化页表项 
+        mov ebx, 0x00021000         ; 页表的物理地址
+        xor eax, eax                ; 起始页的物理地址 
+        xor esi, esi
+
+    .b1:
+        mov edx, eax
+        or edx, 0x00000003             
+        mov [ebx + esi * 4], edx    ; 登记页的物理地址
+        add eax, 0x1000             ; 下一个相邻页的物理地址 
+        inc esi
+        cmp esi, 256                ; 仅低端1MB内存对应的页才是有效的 
+        jl .b1
+
+    ; 令CR3寄存器指向页目录，并正式开启页功能 
+    mov eax, 0x00020000             ; PCD = PWT = 0
+    mov cr3, eax
+
+    ; 将GDT的线性地址映射到从0x80000000开始的相同位置 
+    sgdt [pgdt]
+    mov ebx, [pgdt + 2]
+    add dword [pgdt + 2], 0x80000000; GDTR也用的是线性地址
+    lgdt [pgdt]
+
+    mov eax, cr0
+    or eax, 0x80000000
+    mov cr0, eax                    ; 开启分页机制
+
+    ;将堆栈映射到高端，这是非常容易被忽略的一件事。应当把内核的所有东西
+    ;都移到高端，否则，一定会和正在加载的用户任务局部空间里的内容冲突，
+    ;而且很难想到问题会出在这里。 
+    add esp, 0x80000000
+
+    jmp [0x80040004]
+
+pgdt dw 0
+     dd 0x00008000                  ; GDT的物理/线性地址
+
+times 510-($-$$) db 0
 db 0x55, 0xaa
