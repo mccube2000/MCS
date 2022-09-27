215a10c0a0edb449acba531bc1eef38ce77870d8
MC_cubes <mccube2000@outlook.com>
2022-09-28 00:42:05 +0800

    pdt & pte init. now can run paging will!!!!!!

--- src/boot/x86/bootsect.asm
+++ src/boot/x86/bootsect.asm
@@ -34,8 +34,8 @@ section mbr vstart=0x7c00
     mov ax, 0x4f00
     int 0x10
     cmp ax, 0x004f
-    ; jne scrn320
-    je scrn320
+    jne scrn320
+    ; je scrn320
     
     ; 检查VBE的版本
     mov ax, [es: di + 4]
@@ -127,56 +127,58 @@ flush:
         inc eax
         loop @2                     ; 循环读
 
-    ; jmp core_base_address
+; 准备页表，一级页表
+    mov ebx, 0x00010000             ; 页目录表PDT的物理地址
+    mov edx, ebx
+    or edx, 0x00000003
+    mov [ebx + 0xffc], edx          ; 对应页目录表自己的目录项
 
-         ;创建系统内核的页目录表PDT
-         mov ebx,0x00020000                 ;页目录表PDT的物理地址
-         
-         ;在页目录内创建指向页目录表自己的目录项
-         mov dword [ebx+4092],0x00020003 
+    mov ecx, 0x3ff
+    mov eax, 0x40f003
+    sub ebx, 4                      ; 页目录表PDT的物理地址 - 4
+    cpd:
+        mov [ebx + ecx * 4], eax    ; 从第0项到第0x3fe项
+        sub eax, 0x1000
+        loop cpd
 
-         mov edx,0x00021003                 ;MBR空间有限，后面尽量不使用立即数
-         ;在页目录内创建与线性地址0x00000000对应的目录项
-         mov [ebx+0x000],edx                ;写入目录项（页表的物理地址和属性）      
-                                            ;此目录项仅用于过渡。
-         ;在页目录内创建与线性地址0x80000000对应的目录项
-         mov [ebx+0x800],edx                ;写入目录项（页表的物理地址和属性）
-        ;  mov [ebx+0xe00],edx                ;写入目录项（页表的物理地址和属性）
+    mov ebx, 0x00011000             ; 初始化第一个目录项，对应0-0x3ff_fff物理内存
+    mov ecx, 0x00000003
+    .b0
+        xor eax, eax
+        xor esi, esi
+        .b1:
+            mov edx, eax
+            or edx, ecx
+            mov [ebx + esi * 4], edx;登记页的物理地址
+            add eax, 0x1000         ; 下一个相邻页的物理地址
+            inc esi
+            cmp esi, 0x400          ; 仅低端1MB内存对应的页才是有效的
+            jl .b1
+    cmp ecx, 0x00000003             ; 显存页初始化完成后，跳转至pgo
+    jne pgo
+    mov ebx, 0x00391000             ; 显存对应页
+    mov ecx, 0xe0000003             ; 显存物理地址0xe0000_000
+    jmp .b0
 
-         ;创建与上面那个目录项相对应的页表，初始化页表项 
-         mov ebx,0x00021000                 ;页表的物理地址
-         xor eax,eax                        ;起始页的物理地址 
-         xor esi,esi
-  .b1:       
-         mov edx,eax
-         or edx,0x00000003                                                      
-         mov [ebx+esi*4],edx                ;登记页的物理地址
-         add eax,0x1000                     ;下一个相邻页的物理地址 
-         inc esi
-         cmp esi,0x100                      ;仅低端1MB内存对应的页才是有效的 
-         jl .b1
-         
-         ;令CR3寄存器指向页目录，并正式开启页功能 
-         mov eax,0x00020000                 ;PCD=PWT=0
-         mov cr3,eax
+pgo:
+    ; 令CR3寄存器指向页目录，并正式开启页功能 
+    mov eax, 0x00010000             ; PCD = PWT = 0
+    mov cr3, eax
 
          ;将GDT的线性地址映射到从0x80000000开始的相同位置 
-         sgdt [gdt]
-         mov ebx,[gdt+2]
-         add dword [gdt+2],0x80000000      ;GDTR也用的是线性地址
-         lgdt [gdt]
+        ;  sgdt [gdt]
+        ;  mov ebx,[gdt+2]
+        ;  add dword [gdt+2],0x80000000      ;GDTR也用的是线性地址
+        ;  lgdt [gdt]
 
-         mov eax,cr0
-         or eax,0x80000000
-         mov cr0,eax                        ;开启分页机制
-   
-         ;将堆栈映射到高端，这是非常容易被忽略的一件事。应当把内核的所有东西
-         ;都移到高端，否则，一定会和正在加载的用户任务局部空间里的内容冲突，
-         ;而且很难想到问题会出在这里。 
-         add esp,0x80000000
-                                             
-         jmp 0x80040000
+    mov eax,cr0
+    or eax,0x80000000
+    mov cr0,eax                        ;开启分页机制
 
+    jmp core_base_address
+    hh:
+        hlt
+        jmp hh
 read_hard_disk_0:
     ; EAX=逻辑扇区号
     ; DS:EBX=目标缓冲区地址
